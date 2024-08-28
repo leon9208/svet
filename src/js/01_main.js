79828c5d3de7a3796db1702956d24a07be3e3c96
Shakhrom Mukumov <leon9208@mail.ru>
2024-08-29 00:06:26 +0300

    popover/tooltip

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -149,3 +149,6 @@ if(dropdownCloseBtn.length > 0) {
 		})
 	})
 };
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
